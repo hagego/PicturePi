3d416e6016d60f28a66409a0829f32c64cd81973
hagen.goller@web.de <hagen.goller@web.de>
2018-11-02 22:25:58 +0100

    externalized TextWatchPanel strings into message.properties file

--- src/picturepi/TextWatchProvider.java
+++ src/picturepi/TextWatchProvider.java
@@ -23,7 +23,7 @@ public class TextWatchProvider extends Provider {
 				textWatchpanel = (TextWatchPanel)panel;
 			}
 			else {
-				log.severe("Panel is not of class TextWatchPanel. Disabling updates.");
+				log.severe("Panel is not of class TextWatchPanel. Disabling updates."); //$NON-NLS-1$
 				
 				return;
 			}
@@ -42,13 +42,13 @@ public class TextWatchProvider extends Provider {
 		
 		if(index!=lastIndex) {
 			if(index >= timeMessages.length) {
-				log.severe("index into time messages out of bounds: "+index);
+				log.severe("index into time messages out of bounds: "+index); //$NON-NLS-1$
 				return;
 			}
 			
 			String msg = timeMessages[index];
-			msg = msg.replace("%current", String.valueOf(time.get(ChronoField.CLOCK_HOUR_OF_AMPM)));
-			msg = msg.replace("%next", String.valueOf(time.get(ChronoField.CLOCK_HOUR_OF_AMPM)+1));
+			msg = msg.replace("%current", String.valueOf(time.get(ChronoField.CLOCK_HOUR_OF_AMPM))); //$NON-NLS-1$
+			msg = msg.replace("%next", String.valueOf(time.get(ChronoField.CLOCK_HOUR_OF_AMPM)+1)); //$NON-NLS-1$
 			
 			textWatchpanel.setTimeText(msg);
 			lastIndex = index;
@@ -61,12 +61,22 @@ public class TextWatchProvider extends Provider {
 	private static final Logger   log     = Logger.getLogger( TextWatchPanel.class.getName() );
 	
 	// time text messages
-	private final String timeMessages[]   = {"kurz nach %current","5 Minuten nach %current","10 Minuten nach %current","viertel %next",
-            "10 Minuten bis halb %next","5 Minuten bis halb %next","halb %next",
-            "5 Minuten nach halb %next","10 Minuten nach halb %next","dreiviertel %next",
-            "10 Minuten bis %next","5 Minuten bis %next","kurz vor %next"};
+	private final String timeMessages[]   = {
+			Messages.getString("TextWatchProvider.TIME_SHORTLY_AFTER_CURRENT"), //$NON-NLS-1$
+			Messages.getString("TextWatchProvider.TIME_5_MINUTES_AFTER_CURRENT"), //$NON-NLS-1$
+			Messages.getString("TextWatchProvider.TIME_10_MINUTES_AFTER_CURRENT"), //$NON-NLS-1$
+			Messages.getString("TextWatchProvider.TIME_QUARTER_PAST_CURRENT"),  //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_10_MINUTES_BEFORE_HALF"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_5_MINUTES_BEFORE_HALF"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_HALF"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_5_MINUTES_PAST_HALF"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_10_MINUTES_PAST_HALF"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_QUARTER_TO_NEXT"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_10_MINUTES_BEFORE_NEXT"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_5_MINUTES_BEFORE_NEXT"), //$NON-NLS-1$
+            Messages.getString("TextWatchProvider.TIME_SHORTLY_BEFORE_NEXT")}; //$NON-NLS-1$
 
 	private       TextWatchPanel    textWatchpanel = null;    // TextWatchPanel to update
 	private       int               lastIndex = -1;           // stores index of last message displayed
-	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); //$NON-NLS-1$
 }
